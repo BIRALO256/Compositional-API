fbe92b2097a994a7511f869ecbd93254ed9c7370
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-06-07 00:38:29 +0300

    learnt some basics in compostional API

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,58 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  
+  <h1>dealing with compositional API at home page</h1>
+  <p>my name is {{name}} and my age is {{ age }}</p>
+  <br>
+  <button @click="age++">add age + 1</button>
+  <br>
+  <button @click="handleclick">click me</button>
+  <br>
+  <lable>Name:</lable>
+  <input type="text" v-model="search" >
+
+  <div v-for="namez in matchingNames" :key="namez" >{{namez}}</div>
+
+  <br>
+  <label>Serch:</label>
+  <input type="text" v-model="search">
+  <p>serch value : {{ search }}</p>
+
   <router-view/>
 </template>
+
+<script>
+import { computed, ref } from 'vue'
+
+
+export default {
+  //the setup function runs first befor anyother function runs on this component 
+  setup() {
+   const name1 =computed(() => {
+      return 'biralo'
+   })
+
+    let name =ref('jovic') // just because it's constant it does not mean the value can't cahange , it can .. it's just the refernce(address) of the value that can't change
+    let age =ref(30)
+
+    const names = ref(['mali', 'sadat','omo', 'imelida', 'tumwebaze','biralo', 'patric'])
+
+  const handleclick = () =>{
+    name.value ='biralo'
+  }
+
+  //computed data ... we are using the available name to return the serched name
+
+  const  search =ref('')
+  const  matchingNames = computed(()=>{
+    return names.value.filter((namez) => namez.includes(search.value))
+  })
+
+    return {name, age,handleclick, names, search, matchingNames}
+    // returning the object of the data we want to use in the component 
+  },
+}
+</script>
+
 
 <style>
 #app {
@@ -15,16 +63,4 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1 class="bg-black" >This is an about page</h1>
-  </div>
-</template>
